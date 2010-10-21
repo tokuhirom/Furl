410dd2768f698e03652900841046c489d57e74d3
Fuji, Goro <gfuji@cpan.org>
2010-10-21 18:41:15 +0900

    Ad an example script: get.pl

---
+++ example/get.pl
@@ -0,0 +1,9 @@
+#!perl -w
+use strict;
+use Furl;
+use HTTP::Response;
+
+my $uri = shift(@ARGV) or die "Usage: $0 URI\n";
+
+my $furl = Furl->new();
+print HTTP::Response->new( $furl->get($uri) )->as_string;
