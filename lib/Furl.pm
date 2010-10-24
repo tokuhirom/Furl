65a7b39e5909c98c6858ec9e8d3c547225c9fd12
tokuhirom <tokuhirom@gmail.com>
2010-10-24 16:57:33 +0900

    typo

--- lib/Furl.pm
+++ lib/Furl.pm
@@ -833,7 +833,7 @@ Loads proxy settings from C<< $ENV{HTTP_PROXY} >>.
 =head1 INTEGRATE WITH HTTP::Response
 
 Some useful libraries require HTTP::Response instances for their arguments.
-You can easily create its instance from the result of C<request()> and other HTTP reqeust methods.
+You can easily create its instance from the result of C<request()> and other HTTP request methods.
 
     my $res = HTTP::Response->new($furl->get($url));
 
