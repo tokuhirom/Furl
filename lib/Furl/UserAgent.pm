4436f1d51abb86ba1711211ee33950d1ba66ddac
tokuhirom <tokuhirom@gmail.com>
2010-10-29 23:27:36 +0900

    header_format => FORMAT_HASHREF makes good performance on F::UserAgent!

--- lib/Furl/UserAgent.pm
+++ lib/Furl/UserAgent.pm
@@ -5,6 +5,11 @@ use utf8;
 use base qw/Furl/;
 use Furl::Response;
 
+sub new {
+    my $class = shift;
+    return $class->SUPER::new(header_format => Furl::FORMAT_HASHREF(), @_);
+}
+
 no strict 'refs';
 for my $meth (qw/get post put delete request/) {
     *{__PACKAGE__ . "::" . $meth} = sub {
