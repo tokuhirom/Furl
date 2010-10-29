9cf2a06ee9aed52232effdeb432f5a6668f42636
tokuhirom <tokuhirom@gmail.com>
2010-10-29 19:22:19 +0900

    rewreite profiling script for simple.pl

--- benchmark/profile.pl
+++ benchmark/profile.pl
@@ -1,14 +1,21 @@
 use strict;
 use warnings;
-use Furl;
+use Furl qw/HEADER_NONE HEADERS_AS_ARRAYREF/;
+use URI;
 
 my $url = shift @ARGV || 'http://127.0.0.1:80/';
+my $uri = URI->new($url);
+my $host       = $uri->host;
+my $port       = $uri->port;
+my $path_query = $uri->path_query;
 
-my $furl = Furl->new(parse_header => 0);
+my $furl = Furl->new(header_format => HEADER_NONE);
 for (1..1000) {
-    my ($code, $headers, $content) = $furl->request(
-        method  => 'GET',
-        url     => $url,
+    my ( $code, $headers, $content ) = $furl->request(
+        method     => 'GET',
+        host       => $host,
+        port       => $port,
+        path_query => $path_query,
     );
     $code == 200 or die "oops : $code, $content";
 }
