3536821b9e8489c17ac966a52e91f2d014d77160
tokuhirom <tokuhirom@gmail.com>
2010-11-02 05:15:40 +0900

    do not use Starman in testing script.

--- t/100_low/21_keep_alive_timedout.t
+++ t/100_low/21_keep_alive_timedout.t
@@ -1,32 +1,17 @@
 #!perl -w
 use strict;
 use warnings;
-use Test::Requires qw(Starman);
 use Furl::HTTP;
 use Test::TCP;
 use Test::More;
-
-use t::SilentStarman;
-#BEGIN{ $ENV{STARMAN_DEBUG} = 1 }
-
-{
-    package Timedout::Server;
-    use base qw(Starman::Server);
-
-    sub _finalize_response {
-        my $self = shift;
-        my $res  = $self->SUPER::_finalize_response(@_);
-        close $self->{server}{client};
-        return $res;
-    }
-}
+use t::HTTPServer;
 
 my $n = shift(@ARGV) || 3;
 
 test_tcp(
     client => sub {
         my $port = shift;
-        my $furl = Furl::HTTP->new();
+        my $furl = Furl::HTTP->new(timeout => 1);
         for (1 .. $n) {
             note "request/$_";
             my ( undef, $code, $msg, $headers, $content ) =
@@ -36,28 +21,28 @@ test_tcp(
                     host       => '127.0.0.1',
                 );
             is $code, 200, "request()/$_";
-            is $msg, "OK";
-            is Furl::HTTP::_header_get($headers, 'Content-Length'), 4, 'header'
+            is $msg, "200";
+            is Furl::HTTP::_header_get($headers, 'Content-Length'), 2, 'header'
                 or diag(explain($headers));
             is Furl::HTTP::_header_get($headers, 'Connection'), 'keep-alive';
-            is $content, '/foo'
+            is $content, 'OK'
                 or do{ require Devel::Peek; Devel::Peek::Dump($content) };
         }
         done_testing;
     },
     server => sub {
         my $port = shift;
-        Timedout::Server->new(
-        )->run(sub {
-            my $env = shift;
-            return [ 200,
-                [ 'Content-Length' => length($env->{REQUEST_URI}) ],
-                [$env->{REQUEST_URI}]
-            ];
-        }, {
-            host      => '127.0.0.1',
-            port      => $port,
-            keepalive => 1,
+        my $server = t::HTTPServer->new(port => $port);
+        $server->add_trigger("AFTER_HANDLE_REQUEST" => sub {
+            my ($s, $csock) = @_;
+            $csock->close();
+        });
+        $server->run(sub {
+            +[
+                200,
+                ['Content-Length' => 2, 'Connection' => 'keep-alive'],
+                ['OK']
+            ]
         });
     }
 );
---
+++ t/HTTPServer.pm
@@ -0,0 +1,213 @@
+package t::HTTPServer;
+use strict;
+use warnings;
+use IO::Socket::INET;
+use Socket qw(IPPROTO_TCP TCP_NODELAY);
+use Carp ();
+
+sub new {
+    my $class = shift;
+    my %args = @_ == 1 ? %{$_[0]} : @_;
+    $args{port} || Carp::croak("missing mandatory parameter 'port'");
+    bless {
+        bufsize => 10*1024,
+        protocol => "HTTP/1.1",
+        %args
+    }, $class;
+}
+
+sub add_trigger {
+    my ($self, $name, $code) = @_;
+    push @{$self->{triggers}->{$name}}, $code;
+}
+
+sub call_trigger {
+    my ($self, $name, @args) = @_;
+    for my $code (@{ $self->{triggers}->{$name} || +[] }) {
+        $code->($self, @args);
+    }
+}
+
+sub run {
+    my ( $self, $app ) = @_;
+
+    local $SIG{PIPE} = "IGNORE";
+    my $sock = IO::Socket::INET->new(
+        Listen    => SOMAXCONN,
+        Proto     => 'tcp',
+        ReuseAddr => 1,
+        LocalAddr => '127.0.0.1',
+        LocalPort => $self->{port},
+        Timeout   => 3,
+    ) or die $!;
+    $sock->autoflush(1);
+    while ( my $csock = $sock->accept ) {
+        $csock->setsockopt( IPPROTO_TCP, TCP_NODELAY, 1 )
+          or die "setsockopt(TCP_NODELAY) failed:$!";
+        eval {
+            $self->handle_connection($csock => $app);
+        };
+        print STDERR "# $@" if $@;
+    }
+}
+
+sub make_header {
+    my ($self, $code, $headers) = @_;
+    my $ret = "$self->{protocol} $code $code\015\012";
+    for (my $i=0; $i<@$headers; $i+=2) {
+        $ret .= $headers->[$i] . ': ' . $headers->[$i+1] . "\015\012";
+    }
+    return $ret;
+}
+
+sub handle_connection {
+    my ($self, $csock, $app) = @_;
+
+    my $buf;
+    my %env;
+    $self->call_trigger( "BEFORE_HANDLE_CONNECTION", $csock );
+    HANDLE_LOOP: while (1) {
+        $self->call_trigger( "BEFORE_HANDLE_REQUEST", $csock );
+      PARSE_HTTP_REQUEST: while (1) {
+            my $nread = sysread( $csock, $buf, $self->{bufsize}, length($buf) );
+            $buf =~ s!^(\015\012)*!! if defined($buf); # for keep-alive
+            if ( !defined $nread ) {
+                die "cannot read HTTP request header: $!";
+            }
+            if ( $nread == 0 ) {
+                die "unexpected EOF while reading HTTP request header";
+            }
+            my $ret = parse_http_request( $buf, \%env );
+            if ( $ret == -2 ) {    # incomplete.
+                next;
+            }
+            elsif ( $ret == -1 ) {    # request is broken
+                die "broken HTTP header";
+            }
+            else {
+                $buf = substr( $buf, $ret );
+                last PARSE_HTTP_REQUEST;
+            }
+        }
+        my $res = $app->( \%env );
+        my $res_header =
+          $self->make_header( $res->[0], $res->[1] ) . "\015\012";
+        $self->write_all( $csock, $res_header );
+        for my $body (@{$res->[2]}) {
+            $self->write_all( $csock, $body );
+        }
+        $self->call_trigger( "AFTER_HANDLE_REQUEST", $csock );
+        warn "# sent";
+    }
+    $self->call_trigger( "AFTER_HANDLE_CONNECTION", $csock );
+}
+
+sub write_all {
+    my ( $self, $csock, $buf ) = @_;
+    my $off = 0;
+    while ( my $len = length($buf) - $off ) {
+        my $nwrite = syswrite( $csock, $buf, $len, $off )
+            or die "Cannot write response: $!";
+        $off += $nwrite;
+    }
+    return $off;
+}
+
+sub parse_http_request {
+    my ( $chunk, $env ) = @_;
+    Carp::croak("second param to parse_http_request should be a hashref")
+      unless ( ref $env || '' ) eq 'HASH';
+
+    # pre-header blank lines are allowed (RFC 2616 4.1)
+    $chunk =~ s/^(\x0d?\x0a)+//;
+    return -2 unless length $chunk;
+
+    # double line break indicates end of header; parse it
+    if ( $chunk =~ /^(.*?\x0d?\x0a\x0d?\x0a)/s ) {
+        return _parse_header( $chunk, length $1, $env );
+    }
+    return -2;    # still waiting for unknown amount of header lines
+}
+
+sub _parse_header {
+    my($chunk, $eoh, $env) = @_;
+
+    my $header = substr($chunk, 0, $eoh,'');
+    $chunk =~ s/^\x0d?\x0a\x0d?\x0a//;
+
+    # parse into lines
+    my @header  = split /\x0d?\x0a/,$header;
+    my $request = shift @header;
+
+    # join folded lines
+    my @out;
+    for(@header) {
+        if(/^[ \t]+/) {
+            return -1 unless @out;
+            $out[-1] .= $_;
+        } else {
+            push @out, $_;
+        }
+    }
+
+    # parse request or response line
+    my $obj;
+    my ($major, $minor);
+
+    my ($method,$uri,$http) = split / /,$request;
+    return -1 unless $http and $http =~ /^HTTP\/(\d+)\.(\d+)$/i;
+    ($major, $minor) = ($1, $2);
+
+    my($path, $query) = ( $uri =~ /^([^?]*)(?:\?(.*))?$/s );
+    # following validations are just needed to pass t/01simple.t
+    if ($path =~ /%(?:[0-9a-f][^0-9a-f]|[^0-9a-f][0-9a-f])/i) {
+        # invalid char in url-encoded path
+        return -1;
+    }
+    if ($path =~ /%(?:[0-9a-f])$/i) {
+        # partially url-encoded
+        return -1;
+    }
+
+    $env->{REQUEST_METHOD}  = $method;
+    $env->{REQUEST_URI}     = $uri;
+    $env->{SERVER_PROTOCOL} = "HTTP/$major.$minor";
+    $env->{PATH_INFO}    = uri_unescape($path);
+    $env->{QUERY_STRING} = $query || '';
+    $env->{SCRIPT_NAME}  = '';
+
+    # import headers
+    my $token = qr/[^][\x00-\x1f\x7f()<>@,;:\\"\/?={} \t]+/;
+    my $k;
+    for my $header (@out) {
+        if ( $header =~ s/^($token): ?// ) {
+            $k = $1;
+            $k =~ s/-/_/g;
+            $k = uc $k;
+
+            if ($k !~ /^(?:CONTENT_LENGTH|CONTENT_TYPE)$/) {
+                $k = "HTTP_$k";
+            }
+        } elsif ( $header =~ /^\s+/) {
+            # multiline header
+        } else {
+            return -1;
+        }
+
+        if (exists $env->{$k}) {
+            $env->{$k} .= ", $header";
+        } else {
+            $env->{$k} = $header;
+        }
+    }
+
+    return $eoh;
+}
+
+sub uri_unescape {
+    local $_ = shift;
+    $_ =~ s/%([0−9A−Fa−f]{2})/chr(hex($1))/eg;
+    $_;
+}
+
+1;
