d7e81dba5f449d4e3530e15ebdb89ffaacc50995
tokuhirom <tokuhirom@gmail.com>
2013-07-01 12:14:47 +0900

    Checking in changes prior to tagging of version 2.17.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index f3e6ef1..6c3a08d 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Furl
    
     {{$NEXT}}
    
    +2.17 2013-07-01T03:11:49Z
    +
    +    - Fix Furl::Request#as_string the request contains ARRAY or HASH.
    +      (tokuhirom)
    +
     2.16 2013-05-22T07:48:21Z
    
         - Fix timeout problem(#56) on recent Linux(3.8 or later)

--- lib/Furl.pm
+++ lib/Furl.pm
@@ -6,7 +6,7 @@ use Furl::HTTP;
 use Furl::Request;
 use Furl::Response;
 use Carp ();
-our $VERSION = '2.16';
+our $VERSION = '2.17';
 
 use 5.008001;
 
--- lib/Furl/HTTP.pm
+++ lib/Furl/HTTP.pm
@@ -4,7 +4,7 @@ use warnings;
 use base qw/Exporter/;
 use 5.008001;
 
-our $VERSION = '2.16';
+our $VERSION = '2.17';
 
 use Carp ();
 use Furl::ConnectionCache;
